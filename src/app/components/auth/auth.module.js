718d73ea261f69b9ba156eaee15b5946bddadf68
thayer11 <holly.coddington@gmail.com>
2016-12-19 11:22:09 -0700

    login/registration

--- src/app/components/auth/auth.module.js
+++ src/app/components/auth/auth.module.js
@@ -2,4 +2,24 @@ angular
   .module('components.auth', [
     'ui.router',
     'firebase'
-  ]);
+  ])
+  
+  .config(function($firebaseRefProvider) {
+  	
+
+  	var config = {
+	    apiKey: "AIzaSyCd6RbQKPicFJWyTdyImx3lEWF2c0MCLhk",
+	    authDomain: "contacts-manager-67874.firebaseapp.com",
+	    databaseURL: "https://contacts-manager-67874.firebaseio.com",
+	    storageBucket: "contacts-manager-67874.appspot.com",
+	    messagingSenderId: "525437099580"
+	  };
+  	
+  	$firebaseRefProvider
+  		.registerUrl({
+  			default: config.databaseURL,
+  			contacts: config.databaseURL + '/contacts'
+  		});
+  	
+  	firebase.initializeApp(config);
+  });
